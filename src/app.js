68a3935133bc0948197e1c38e30bb5777e406c75
lqr131115 <1046975211@qq.com>
2021-09-21 17:07:38 +0800

    feat: 404和错误界面

--- src/app.js
+++ src/app.js
@@ -5,36 +5,27 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
-
+const { isPro } = require('./utils/env')
 const index = require('./routes/index')
-const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
 // error handler
-onerror(app)
+let onErrorConfig = {}
+// 线上环境重定向到错误界,开发环境显示错误即可 
+isPro && (onErrorConfig = {redirect: '/error'})
+onerror(app, onErrorConfig)
 
 // middlewares
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
+app.use(bodyparser({ enableTypes: ['json', 'form', 'text'] }))
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+app.use(views(__dirname + '/views', { extension: 'ejs' }))
 
 // routes
 app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+// 404  路由必须放在最后
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
